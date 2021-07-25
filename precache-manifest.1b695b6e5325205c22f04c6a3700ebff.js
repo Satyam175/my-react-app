96fac43c974d66c0f41b2dc19d34971b3add47b9
Satyam175 <61234615+Satyam175@users.noreply.github.com>
2021-07-25 16:37:57 +0530

    Updates

---
+++ precache-manifest.1b695b6e5325205c22f04c6a3700ebff.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09988a6fbea57000bf82fb5343ef6a83",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "5ac49a6da523e53205f7",
+    "url": "/my-react-app/static/css/3.bc85b1b3.chunk.css"
+  },
+  {
+    "revision": "1df227f8eb5fde15b0ba",
+    "url": "/my-react-app/static/css/4.5c57288c.chunk.css"
+  },
+  {
+    "revision": "e8909ab14fa4b2d570c9",
+    "url": "/my-react-app/static/css/main.3bb04327.chunk.css"
+  },
+  {
+    "revision": "d4cf72576b0cc878c0d4",
+    "url": "/my-react-app/static/js/2.aeb5078b.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/my-react-app/static/js/2.aeb5078b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ac49a6da523e53205f7",
+    "url": "/my-react-app/static/js/3.1dc30e87.chunk.js"
+  },
+  {
+    "revision": "1df227f8eb5fde15b0ba",
+    "url": "/my-react-app/static/js/4.1a0b07b3.chunk.js"
+  },
+  {
+    "revision": "e8909ab14fa4b2d570c9",
+    "url": "/my-react-app/static/js/main.6cda4125.chunk.js"
+  },
+  {
+    "revision": "71b0f5eeb8a07e2ea48e",
+    "url": "/my-react-app/static/js/runtime-main.fb7e501b.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/my-react-app/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/my-react-app/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/my-react-app/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/my-react-app/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/my-react-app/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/my-react-app/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/my-react-app/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/my-react-app/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "d4cd31abcfdd3175bc0cb4f16be991f3",
+    "url": "/my-react-app/static/media/jecLogo.d4cd31ab.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/my-react-app/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/my-react-app/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/my-react-app/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/my-react-app/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/my-react-app/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/my-react-app/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/my-react-app/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
